d9fd110f6977e3a54ac19168380ef99502abdaea
Don <donducdo@gmail.com>
2022-06-28 22:41:57 -0700

    Update Website Styling and Game Logic to User picking pictures

--- index.js
+++ index.js
@@ -4,55 +4,112 @@ let result = document.querySelector(".result")
 let playerScore = document.querySelector(".playerScore")
 let computerScore = document.querySelector(".computerScore")
 
+let playerSide = document.querySelector(".player")
+let compSide = document.querySelector(".computer")
+
+
+// Rock, Paper, Scissor
+
+let rock = document.querySelector(".rock")
+let paper = document.querySelector(".paper")
+let scissor = document.querySelector(".scissor")
+
 
 let hands = ["rock", "paper", "scissor"]
 
 let tie = "It's a Tie!"
 
+let count = 0;
 let round = 1;
 let playerCount = 0;
 let compCount = 0;
 let playerWin = "Player Wins Round " 
 let compWin = "Computer Wins Round " 
 
-function computerPlay(hands){
+let playerSelection = ""
+
+// Random Computer Hand
+
+let computerSelection = function computerPlay(hands){
     let randomChoice = Math.round(Math.random() * 2) 
     let computerHand = hands[randomChoice]
+    // createCompHand(computerHand)
     return computerHand
 }
 
+// Create Computer and Player Hands Pictures
 
-function playRound(playerSelection, computerSelection){
-        if (playerSelection === computerSelection){
-            result.textContent = tie
-        } 
-        else if ((hands.indexOf(playerSelection) - hands.indexOf(computerSelection)) == 1){
-            result.textContent = playerWin + round
-            playerCount++
-            playerScore.textContent = playerCount
-            round++
-        }
-        else if ((hands.indexOf(computerSelection) - hands.indexOf(playerSelection)) == 1){
-            result.textContent = compWin + round
-            compCount++
-            computerScore.textContent = compCount
-            round++
-        }
-        else if ((hands.indexOf(playerSelection) - hands.indexOf(computerSelection)) == 2){
-            result.textContent = compWin + round
-            compCount++
-            computerScore.textContent = compCount
-            round++
-        }
-        else if ((hands.indexOf(computerSelection) - hands.indexOf(playerSelection)) == 2){
-            result.textContent = playerWin + round
-            playerCount++
-            playerScore.textContent = playerCount
-            round++
-        }
-        game()
+function createCompHand(computerHand){
+    let compHand = document.createElement("img")
+    compSide.appendChild(compHand)
+    compHand.classList.add("chosen-hand", "compChoice")
+    compHand.src = computerHand + ".jpg"
 }
 
+function createPlayerHand(){
+    let playerHand = document.createElement("img")
+    playerSide.appendChild(playerHand)
+    playerHand.classList.add("chosen-hand", "compChoice")
+    playerHand.src = "rock.jpg"
+}
+
+// Update Player and Computer Hands
+
+function changeCompHand(computerHand){
+    compHand.src = computerHand + ".jpg"
+}
+
+function changePlayerHand(playerHand){
+    playerHand.src = "rock.jpg"
+}
+
+// Player Hand Choices
+
+function chooseRock(){
+    rock.addEventListener("click", function(){
+        let compChoice = computerSelection(hands)
+        console.log(compChoice)
+        playerSelection = "rock"
+        if (compChoice === "rock"){
+            console.log("tie")
+        }
+        else if (compChoice === "paper"){
+            console.log(compWin + round)
+        }
+        else if (compChoice === "scissor"){
+            console.log(playerWin + round)
+        }
+    })
+}
+
+function choosePaper(){
+    paper.addEventListener("click", function(){
+        playerSelection = "paper"
+    })
+}
+
+function chooseScissor(){
+    scissor.addEventListener("click", function(){
+        playerSelection = "scissor"
+    })
+}
+
+function activateHands(){
+    chooseRock()
+    choosePaper()
+    chooseScissor()
+}
+
+
+// function playRound(playerSelection, computerSelection){
+//         if (playerSelection === computerSelection){
+//             result.textContent = tie
+//             console.log("tie")
+//         } 
+//         game()
+// }
+
+// Game Finish
 
 function game(){
 if (playerCount === 5){
@@ -65,10 +122,30 @@ else if (compCount === 5){
 }
 }
 
+// Reset Game
+
+function resetGame(){
+    round = 1;
+    playerCount = 0;
+    compCount = 0;
+    playButton.textContent = "Play Again"
+}
 
 playButton.addEventListener("click", function(){
-    let computerSelection = computerPlay(hands)
-    let playerChoice = prompt("Please enter rock, paper or scissor: ")
-    let playerSelection = playerChoice.toLowerCase()
-    playRound(playerSelection, computerSelection)
+    result.textContent = "Good Luck! :)"
+    resetGame()
 })
+
+// First Pick
+
+activateHands()
+
+// function beginGame(hands){
+//     activateHands()
+//     if (count == 0){
+//         if (chooseRock() && computerPlay(hands)){
+//             console.log()
+//         }
+//     }
+// }
+
